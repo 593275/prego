6b44f0910dc4f0cf00d4c38941fb5118a5432e45
593275 <69849645+593275@users.noreply.github.com>
2023-03-23 00:02:53 +0100

    css

--- src/Pages/Aruba.jsx
+++ src/Pages/Aruba.jsx
@@ -10,6 +10,11 @@ const Aruba = () => {
       <p className="InntektsgruppeTekst">Høyinntekstsland</p>
       <h3 className="Region">Region:</h3>
       <p className="RegionTekst">Latin America & Caribbean</p>
+      <h3 className="Beskrivelse">Beskrivelse</h3>
+      <p className="BeskrivelseTekst">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <Navbar />
     </div>
   );
