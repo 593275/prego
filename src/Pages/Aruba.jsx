818b521decfa0999c7b1e4355ac9e60d7f7fab54
593275 <69849645+593275@users.noreply.github.com>
2023-03-23 15:52:34 +0100

    flagg

--- src/Pages/Aruba.jsx
+++ src/Pages/Aruba.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./Navbar";
 import "../css/Aruba.css";
+import myImage from "../Bilder/Flag_of_Aruba.svg.png"
 
 const Aruba = () => {
   return (
@@ -15,6 +16,7 @@ const Aruba = () => {
       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
       Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
       Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <img className="Bilde" src={myImage} alt="My Image" />
       <Navbar />
     </div>
   );
