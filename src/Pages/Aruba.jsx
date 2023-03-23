52f912ee2db20b5987a7b88f322895feb8a7109d
593275 <69849645+593275@users.noreply.github.com>
2023-03-24 00:55:02 +0100

    css

--- src/Pages/Aruba.jsx
+++ src/Pages/Aruba.jsx
@@ -17,10 +17,10 @@ const Aruba = () => {
       Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
       Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <div className="circles-container">
-      <div className="circle red"></div>
-      <div className="circle green"></div>
-      <div className="circle orange"></div>
-    </div>
+        <div className="circle red"></div>
+        <div className="circle green"></div>
+        <div className="circle orange"></div>
+      </div>
       <img className="Bilde" src={myImage} alt="My Image" />
       <Navbar />
       
