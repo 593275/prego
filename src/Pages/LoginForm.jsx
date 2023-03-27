6fa161ec9df2c5348419432f38d1441041ff1c2e
593275 <69849645+593275@users.noreply.github.com>
2023-03-27 21:41:15 +0200

    Login funker

--- src/Pages/LoginForm.jsx
+++ src/Pages/LoginForm.jsx
@@ -1,20 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { db } from "../config/firebase-config"
+import { getDoc, doc, collection, onSnapshot } from "firebase/firestore";
 import "../css/loginForm.css"
 
 const LoginForm = () => {
   const navigate = useNavigate();
+  const [users, setUsers] = useState([])
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    
-    console.log("hei")
-      
-    navigate("/dashboard");
-    
-  };
+  useEffect(
+    () => 
+      onSnapshot(collection(db, "Users"), (snapshot) => 
+        setUsers(snapshot.docs.map(doc => ({...doc.data()})))
+      ),
+    []
+    );
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+     console.log(users)
+      const user = users.find((u) => u.Username === username && u.Password === password);
+      if (user) {
+        if (user.username === "admin") {
+          navigate("/admin/dashboard");
+        } else {
+          navigate("/dashboard");
+        }
+      } else {
+        setError("Invalid username or password");
+      }
+    };
 
   return (
     <form onSubmit={handleSubmit}>
@@ -26,6 +44,7 @@ const LoginForm = () => {
         <label htmlFor="password">Password: </label>
         <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
       </div>
+      {error && <p>{error}</p>}
       <button type="submit">Submit</button>
     </form>
   );
