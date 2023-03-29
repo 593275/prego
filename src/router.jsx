7097fe5610d7278443b6c5584c233a626289640e
Your Name <you@example.com>
2023-03-29 11:59:12 +0200

    private routes

--- src/router.jsx
+++ src/router.jsx
@@ -1,13 +1,28 @@
-import { createBrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { PrivateRoutes } from './Functions/privateRoutes';
 import Login from './Pages/LoginForm';
 import Dashboard from './Pages/Dashboard';
 import OmPreGO from "./Pages/OmPreGO";
 import AndreFaktorer from "./Pages/AndreFaktorer";
 import Aruba from "./Pages/Aruba";
 
-
-export const router = createBrowserRouter([{path: "/", element: <Login/>}, 
-                                           {path: "/dashboard", element: <Dashboard/>},
-                                           {path: "/omprego", element: <OmPreGO/>},
-                                           {path: "/andrefaktorer", element: <AndreFaktorer/>},
-                                           {path: "/Aruba", element: <Aruba/>}])
+  const App = () =>  {
+    return (
+      <div className="App">
+          <Router>
+            <Routes>
+              <Route element={<PrivateRoutes />}>
+                  <Route element={<Dashboard/>} path="/dashboard" exact/>
+                  <Route element={<OmPreGO/>} path="/OmPreGo"/>
+                  <Route element={<AndreFaktorer/>} path="/AndreFaktorer"/>
+                  <Route element={<Aruba/>} path="/Aruba"/>
+              </Route>
+              <Route element={<Login/>} path="/"/>
+            </Routes>
+        </Router>
+      </div>
+    );
+  }
+  
+  export default App;
+  
