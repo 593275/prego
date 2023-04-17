b2da44185f54727352a5527843c3ba4b7acc7a69
593275 <69849645+593275@users.noreply.github.com>
2023-04-17 14:55:37 +0200

    fixed æøå

--- src/Pages/adminDashboard.jsx
+++ src/Pages/adminDashboard.jsx
@@ -7,34 +7,38 @@ import { landDb } from "../Functions/function"
  function App() {
   const [data, setData] = React.useState(null);
   const [error, setError] = useState('');
-  const dbLand = landDb();
-  console.log(dbLand);
 
   const handleFileUpload =  async (e) => {
+    console.log(1)
     const file = e.target.files[0];
     Papa.parse(file, {
-      encoding: "ISO-8859-1",
+      encoding: "utf-8",
+      delimiter: ";",
       complete: async (results) => {
-        const items = results.data.map((item) => {
+        const items = results.data.slice(1)
+        .filter((item) => item.length === 15) 
+        .map((item) => {
           return {
-            ctry: item.ctry,
-            N: parseInt(item.N),
-            n_sb: parseInt(item.n_sb),
-            pct_sb: parseFloat(item.pct_sb.replace(",", ".")),
-            n_lbw: parseInt(item.n_lbw),
-            pct_lbw: parseFloat(item.pct_lbw.replace(",", ".")),
-            n_pet: parseInt(item.n_pet),
-            pct_pet: parseFloat(item.pct_pet.replace(",", ".")),
-            n_gdm: parseInt(item.n_gdm),
-            pct_gdm: parseFloat(item.pct_gdm.replace(",", ".")),
-            n_cs: parseInt(item.n_cs),
-            pct_cs: parseFloat(item.pct_cs.replace(",", ".")),
-            n_fa: parseInt(item.n_fa),
-            pct_fa: parseFloat(item.pct_fa.replace(",", ".")),
-            gbd: item.gbd,
+            ctry: item[0],
+            N: parseInt(item[1]),
+            n_sb: parseInt(item[2]),
+            pct_sb: parseFloat(item[3].replace(",", ".")),
+            n_lbw: parseInt(item[4]),
+            pct_lbw: parseFloat(item[5].replace(",", ".")),
+            n_pet: parseInt(item[6]),
+            pct_pet: parseFloat(item[7].replace(",", ".")),
+            n_gdm: parseInt(item[8]),
+            pct_gdm: parseFloat(item[9].replace(",", ".")),
+            n_cs: parseInt(item[10]),
+            pct_cs: parseFloat(item[11].replace(",", ".")),
+            n_fa: parseInt(item[12]),
+            pct_fa: parseFloat(item[13].replace(",", ".")),
+            gbd: item[14],
           };
         });
-
+        for (const item of items) {
+          console.log(item)
+        }
         for (const item of items) {
           const docRef = doc(db, "Land", item.ctry)
           const docSnap = await getDoc(docRef)
@@ -75,13 +79,13 @@ import { landDb } from "../Functions/function"
               pct_fa: item.pct_fa,
               gbd: item.gbd
             });
+            
           }
         
         }
 
-        
       },
-      header: true,
+      
     });
   };
 
--- src/Pages/tester.jsx
+++ src/Pages/tester.jsx
@@ -6,9 +6,34 @@ function CsvReader() {
     const file = event.target.files[0];
 
     Papa.parse(file, {
-      encoding: "ISO-8859-1",
+      encoding: "utf-8",
+      delimiter: ";",
       complete: (results) => {
-        console.log(results.data);
+        const items = results.data.slice(1)
+  .filter((item) => item.length === 15) // filter out invalid items
+  .map((item) => {
+    return {
+      ctry: item[0],
+      N: parseInt(item[1]),
+      n_sb: parseInt(item[2]),
+      pct_sb: parseFloat(item[3].replace(",", ".")),
+      n_lbw: parseInt(item[4]),
+      pct_lbw: parseFloat(item[5].replace(",", ".")),
+      n_pet: parseInt(item[6]),
+      pct_pet: parseFloat(item[7].replace(",", ".")),
+      n_gdm: parseInt(item[8]),
+      pct_gdm: parseFloat(item[9].replace(",", ".")),
+      n_cs: parseInt(item[10]),
+      pct_cs: parseFloat(item[11].replace(",", ".")),
+      n_fa: parseInt(item[12]),
+      pct_fa: parseFloat(item[13].replace(",", ".")),
+      gbd: item[14],
+    };
+  });
+  
+  for(const item of items) {
+    console.log(item.N)
+  }
       },
     });
   };
