e9c921e3f89743a89c7165b0c1d5960a9581d8a9
William <593271>
2023-04-19 16:17:43 +0200

    Endring på description

--- src/Pages/AndreFaktorer.jsx
+++ src/Pages/AndreFaktorer.jsx
@@ -5,18 +5,18 @@ const AndreFaktorer = () => {
   const [responses, setResponses] = useState([]);
 
   const questions = [
-    { question: "Spørsmål 1", description: "Årsåk til innvandring: Dette  spørsmålet kartlegger hvorfor personen har immigrert til det aktuelle landet. Det kan inkludere faktorer som arbeidsmuligheter, utdanning, familieforhold, politiske eller humanitære årsaker osv." },
-    { question: "Spørsmål 2", description: "Tid i landet: Dette spørsmålet kartlegger hvor lenge personen har vært i det aktuelle landet, og om de er nyankomne immigranter eller har bodd der i lengre tid." },
-    { question: "Spørsmål 3", description: "Opplevde språkbarrierer: Dette spørsmålet kartlegger om personen har erfart utfordringer med språket i det aktuelle landet, som for eksempel kommunikasjon med helsepersonell, tilgang til helseinformasjon eller forståelse av helsevesenet." },
-    { question: "Spørsmål 4", description: "Sosioøkonomiske forhold: Dette spørsmålet kartlegger personens økonomiske og sosiale status, inkludert faktorer som inntekt, utdanning, arbeidsstatus, og boforhold. Disse faktorene kan påvirke tilgangen til helsetjenester og helsestatus." },
-    { question: "Spørsmål 5", description: "Helsekompetanse: Dette spørsmålet kartlegger personens kunnskap og forståelse av helse og helsevesenet, inkludert forståelse av sykdommer, behandlingsalternativer, og helserelaterte beslutninger." },
-    { question: "Spørsmål 6", description: "Tillit: Dette spørsmålet kartlegger personens tillit til helsevesenet i det aktuelle landet, inkludert tillit til helsepersonell, helseinstitusjoner og helsetjenester. Dette kan påvirke hvorvidt personen søker helsetjenester og følger anbefalt behandling." },
-    { question: "Spørsmål 7", description: "Utfordrende å ta opp en god anamnese: Dette spørsmålet kartlegger om helsepersonell har erfart utfordringer med å innhente en grundig sykehistorie (anamnese) fra pasienten, som kan påvirke diagnostisering og behandlingsbeslutninger." },
-    { question: "Spørsmål 8", description: "Genetiske faktorer: Dette spørsmålet kartlegger om det er noen genetiske faktorer eller familiehistorie av sykdommer eller tilstander som kan påvirke personens helsestatus." },
-    { question: "Spørsmål 9", description: "Sykdommer eller tilstander uvanlige i Norge: Dette spørsmålet kartlegger om personen har noen sykdommer eller tilstander som er uvanlige eller ikke så vanlige i det aktuelle landet sammenlignet med Norge. Dette kan inkludere regionsspesifikke sykdommer, klimarelaterte lidelser eller andre helseutfordringer som kan påvirke personens helsetilstand." },
-    { question: "Spørsmål 10", description: "Utsatt for vold: Dette spørsmålet kartlegger om personen har vært utsatt for vold eller traumer, enten før de immigrerte til det aktuelle landet eller etter ankomst. Dette kan inkludere fysisk vold, psykisk vold, overgrep eller andre traumatiske hendelser som kan påvirke personens fysiske og psykiske helse." },
-    { question: "Spørsmål 11", description: "Papirløs? Dette spørsmålet kartlegger om personen er papirløs, det vil si uten gyldige immigrasjonsdokumenter eller oppholdstillatelse i det aktuelle landet. Dette kan påvirke personens tilgang til helsetjenester og helsehjelp, samt deres generelle helsestatus og livskvalitet." },
-    { question: "Spørsmål 12", description: "Mistanke om suboptimal oppfølging av det offentlige? Dette spørsmålet kartlegger om personen har mistanke om at de ikke får tilstrekkelig oppfølging eller omsorg fra det offentlige helsevesenet i det aktuelle landet. Dette kan inkludere opplevelse av diskriminering, fordommer eller manglende tilgang til helsetjenester på grunn av ulike årsaker." }
+    { question: "Spørsmål 1", description: "Årsåk til innvandring" },
+    { question: "Spørsmål 2", description: "Tid i landet" },
+    { question: "Spørsmål 3", description: "Opplevde språkbarrierer" },
+    { question: "Spørsmål 4", description: "Sosioøkonomiske forhold" },
+    { question: "Spørsmål 5", description: "Helsekompetanse" },
+    { question: "Spørsmål 6", description: "Tillit" },
+    { question: "Spørsmål 7", description: "Utfordrende å ta opp en god anamnese" },
+    { question: "Spørsmål 8", description: "Genetiske faktorer" },
+    { question: "Spørsmål 9", description: "Sykdommer eller tilstander uvanlige i Norge" },
+    { question: "Spørsmål 10", description: "Utsatt for vold" },
+    { question: "Spørsmål 11", description: "Papirløs?" },
+    { question: "Spørsmål 12", description: "Mistanke om suboptimal oppfølging av det offentlige?" }
     ];
 
     const handleResponse = (index, response) => {
