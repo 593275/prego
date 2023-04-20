8d26b313ea5058acc48e143639be45b5ecf6ec5d
William <593271>
2023-04-20 23:25:27 +0200

    Graf-gbd-fix

--- src/Pages/Graph.jsx
+++ src/Pages/Graph.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Bar } from "react-chartjs-2";
 import Navbar from "./Navbar";
 import { useState, useEffect } from "react";
-import { collection, query, doc, getDoc, updateDoc, deleteDoc } from "firebase/firestore";
+import { collection, query, doc, getDoc, updateDoc, deleteDoc, where , getDocs} from "firebase/firestore";
 import { db } from "../config/firebase-config"
 
 import {
@@ -20,27 +20,57 @@ function Tester() {
 
   const [items, setItems] = useState([])
   const [items2, setItems2] = useState([])
-
+  const dataArray = []
 
   useEffect(() => {
-    const getNorge = async () => {
-      const land = "Norge"
-      const docRef = doc(db, "Land", land)
-      const docSnap= await getDoc(docRef)
-      setItems(docSnap.data());
-      console.log(docSnap.data())
-    };
-
-    const getLand = async () => {
+    const getLandData = async () => {
       const land = localStorage.getItem("userInput")
       const docRef = doc(db, "Land", land)
-      const docSnap= await getDoc(docRef)
-      setItems2(docSnap.data());
-      console.log(docSnap.data())
+      const docSnap = await getDoc(docRef)
+      if(docSnap.data().N < 1000) {
+        const landRef = collection(db, "Land");
+        const queryGBD = query(landRef, where("gbd", "==", docSnap.data().gbd))
+        const querySnapshot = await getDocs(queryGBD)
+
+        querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          dataArray.push(data);
+        });
+
+        const totalN = dataArray.reduce((acc, cur) => acc + cur.N, 0);
+        const totalN_sb = dataArray.reduce((acc, cur) => acc + cur.n_sb, 0);
+        const totalN_lbw = dataArray.reduce((acc, cur) => acc + cur.n_lbw, 0);
+        const totalN_pet = dataArray.reduce((acc, cur) => acc + cur.n_pet, 0);
+        const totalN_gdm = dataArray.reduce((acc, cur) => acc + cur.n_gdm, 0);
+        const totalN_cs = dataArray.reduce((acc, cur) => acc + cur.n_cs, 0);
+        const totalN_fa = dataArray.reduce((acc, cur) => acc + cur.n_cs, 0);
+
+        const dataGBD = {
+          pct_sb: (totalN_sb / totalN)*100,
+          pct_lbw: (totalN_lbw / totalN)*100,
+          pct_pet: (totalN_pet / totalN)*100,
+          pct_gdm: (totalN_gdm / totalN)*100,
+          pct_cs: (totalN_cs / totalN)*100,
+          pct_fa: (totalN_fa / totalN)*100
+        }
+
+        setItems2(dataGBD)
+     
+
+      } else {
+        setItems2(docSnap.data());
+      }
     };
 
-    getNorge();
-    getLand();
+    const getNorgeData = async () => {
+      const land = "Norge"
+      const docRef = doc(db, "Land", land)
+      const docSnap = await getDoc(docRef)
+      setItems(docSnap.data());
+    };
+
+    getNorgeData();
+    getLandData();
   }, []);
 
   
@@ -57,7 +87,7 @@ function Tester() {
         borderWidth: 1,
       },
       {
-        label: [items2.ctry],
+        label: [localStorage.getItem("userInput")],
         data: [items2.pct_sb, items2.pct_lbw, items2.pct_pet, items2.pct_gdm, items2.pct_cs, items2.pct_fa],
         backgroundColor: 'rgba(54, 162, 235, 0.2)',
         borderColor: 'rgba(54, 162, 235, 1)',
