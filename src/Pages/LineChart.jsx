047739b67f2db3b32f0a603f570950b2c9f1c4e8
593275 <69849645+593275@users.noreply.github.com>
2023-04-27 15:39:20 +0200

    linechart fix

--- src/Pages/LineChart.jsx
+++ src/Pages/LineChart.jsx
@@ -15,13 +15,16 @@ import {
   
   ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement);
 
-function LineChart() {
+function LineChart(label) {
+    const risiko =label.label
     const [items, setItems] = useState([])
     const [items2, setItems2] = useState([])
     const [items3, setItems3] = useState([])
-    const dataArray = []
     const yearArray = [2001,2005,2009,2013,2017,2021];
     let data = ""
+
+    console.log(risiko)
+    console.log("hei")
   
     useEffect(() => {
 
@@ -34,22 +37,26 @@ function LineChart() {
           const docSnap = await getDoc(docRef)
           landAar[index] = (docSnap.data());
       }
-      setItems3(landAar)
+  
+      const riktigRisiko = landAar.map(item => item[risiko])
+      setItems3(riktigRisiko)
+      
       };
 
       const getLandData = async () => {
 
         const landAar2 = []
-        const land = "Etiopia"
+        const land = localStorage.getItem("userInput")
          for(let index = 0; index < yearArray.length; index ++) {
           const docRef = doc(db, "Land" + yearArray[index], land + yearArray[index])
           const docSnap = await getDoc(docRef)
           landAar2[index] = (docSnap.data());
       }
-      setItems2(landAar2)
-      };
-  
 
+      const riktigRisiko = landAar2.map(item => item[risiko])
+      setItems2(riktigRisiko)
+
+      };
   
       getNorgeData();
       getLandData();
@@ -57,20 +64,21 @@ function LineChart() {
   
    
   if(typeof items3 !== 'undefined' && typeof items2 !== 'undefined') {
+
      data = {
       
       labels: ['2001', '2005', '2009', '2013', '2017', '2021'],
       datasets: [
         {
           label: 'Norge',
-          data: [items3[0]?.pct_sb,items3[1]?.pct_sb,items3[2]?.pct_sb,items3[3]?.pct_sb,items3[4]?.pct_sb],
+          data: [items3[0], items3[1], items3[2], items3[3], items3[4], items3[5]],
           backgroundColor: 'rgba(255, 99, 132, 0.2)',
           borderColor: 'rgba(255, 99, 132, 1)',
           borderWidth: 1,
         },
         {
           label: [localStorage.getItem("userInput")],
-          data: [items2[0]?.pct_sb,items2[1]?.pct_sb,items2[2]?.pct_sb,items2[3]?.pct_sb,items2[4]?.pct_sb],
+          data: [items2[0], items2[1], items2[2], items2[3], items2[4], items2[5]],
           backgroundColor: 'rgba(54, 162, 235, 0.2)',
           borderColor: 'rgba(54, 162, 235, 1)',
           borderWidth: 1,
