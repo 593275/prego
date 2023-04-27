1d3cd8b2abcb10f4da057c03f426a1cd7ccd3dec
William <593271>
2023-04-27 16:06:23 +0200

    Prosent-Fix

--- src/Pages/Graph.jsx
+++ src/Pages/Graph.jsx
@@ -81,20 +81,21 @@ function Tester() {
     datasets: [
       {
         label: 'Norge',
-        data: [items.pct_sb, items.pct_lbw, items.pct_pet, items.pct_gdm, items.pct_cs, items.pct_fa],
+        data: [items.pct_sb.toFixed(1), items.pct_lbw.toFixed(1), items.pct_pet.toFixed(1), items.pct_gdm.toFixed(1), items.pct_cs.toFixed(1), items.pct_fa.toFixed(1)],
         backgroundColor: 'rgba(255, 99, 132, 0.2)',
         borderColor: 'rgba(255, 99, 132, 1)',
         borderWidth: 1,
       },
       {
         label: [localStorage.getItem("userInput")],
-        data: [items2.pct_sb, items2.pct_lbw, items2.pct_pet, items2.pct_gdm, items2.pct_cs, items2.pct_fa],
+        data: [items2.pct_sb.toFixed(1), items2.pct_lbw.toFixed(1), items2.pct_pet.toFixed(1), items2.pct_gdm.toFixed(1), items2.pct_cs.toFixed(1), items2.pct_fa.toFixed(1)],
         backgroundColor: 'rgba(54, 162, 235, 0.2)',
         borderColor: 'rgba(54, 162, 235, 1)',
         borderWidth: 1,
       },
     ]
   };
+  
 
   const options = {};
 
