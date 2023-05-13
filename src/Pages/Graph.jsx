5bb0521ded9c2587ea819a2a97e55532f53a6e70
593275 <69849645+593275@users.noreply.github.com>
2023-05-13 15:55:12 +0200

    mer graf fix

--- src/Pages/Graph.jsx
+++ src/Pages/Graph.jsx
@@ -95,7 +95,7 @@ function Tester() {
   return (
     <div className="App">
       
-      <div className="graf">
+      <div className="graf" >
         <h1>PreGO Graf</h1>
         <Bar data={data} options={options} />
       </div>
